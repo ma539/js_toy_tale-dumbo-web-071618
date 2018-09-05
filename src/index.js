818d2abc4f734e5407f4248aef72a622d780e252
ma539 <ma539@cornell.edu>
2018-09-05 16:47:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,57 +7,94 @@ document.addEventListener("DOMContentLoaded", () => {
   let list = document.querySelector('#toy-collection')
   fetch("http://localhost:3000/toys")
   .then(res => {
-    return res.json()
+  return res.json()
   })
-  .then(json => {
-    json.forEach((toy) => {
-      let toyDiv = document.createElement('div')
-      let toyName = document.createElement('h2')
-      let toyImage = document.createElement('img')
-      let toyLikes = document.createElement('p')
-      let likeButton = document.createElement('button')
+  .then((json) => {
+  json.forEach(renderToy)
 
-      toyDiv.className = 'card'
-      toyImage.className = 'toy-avatar'
-      likeButton.className = 'like-btn'
 
-      toyImage.src = toy.image
-      toyName.innerText = toy.name
-      toyLikes.innerText = toy.likes
+  });
 
-      toyDiv.append(toyName, toyImage, toyLikes, likeButton)
-      list.appendChild(toyDiv)
-    })
-  })
-})
+  const renderToy = (toy) => {
+    let toyDiv = document.createElement('div')
+    let toyName = document.createElement('h2')
+    let toyImage = document.createElement('img')
+    let toyLikes = document.createElement('p')
+    let likeButton = document.createElement('button')
 
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-    // submit listener here
-  } else {
-    toyForm.style.display = 'none'
+    toyDiv.className = 'card'
+    toyDiv.dataset.id = toy.id
+    toyDiv.dataset.likes = toy.likes
+    toyImage.className = 'toy-avatar'
+    likeButton.className = 'like-btn'
+
+
+    toyImage.src = toy.image
+    toyName.innerText = toy.name
+    toyLikes.innerText = toy.likes
+    likeButton.innerText = "Like"
+
+    toyDiv.append(toyName, toyImage, toyLikes, likeButton)
+    list.appendChild(toyDiv)
+
+    likeButton.addEventListener('click', () => {
+        const toyId = event.target.parentNode.dataset.id
+        fetch(`http://localhost:3000/toys/${toyId}`, {
+          method: "PATCH",
+          headers: {"Content-type": "application/json"},
+          body: JSON.stringify({likes: parseInt(event.target.parentNode.dataset.likes) + 1})
+        }).then(res => {return res.json()}).then((json) => {toyLikes.innerText=json.likes; toyLikes.parentNode.dataset.likes = json.likes})
+        });
   }
-})
 
-toyForm.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const newToyName = document.getElementbyId('name')
-  const newToyImage = document.getElementbyId('image')
-  fetch("http://localhost:3000/toys",{
-     method: "POST",
-     headers: {"Content-type": "application/json"},
-     body: JSON.stringify({name: newToyName.value,
-       image: newToyImage.value,
+  addBtn.addEventListener('click', () => {
+    // hide & seek with the form
+    addToy = !addToy
+    if (addToy) {
+      toyForm.style.display = 'block'
+      // submit listener here
+    } else {
+      toyForm.style.display = 'none'
+    }
+  })
+  // function editToy(event){
+  // // console.log("hit");
+  // // // event.preventDefault()
+  // const toyId = event.target.parentNode.dataset.id
+  // console.log(event)
+  // fetch(`http://localhost:3000/toys/${toyId}`, {
+  //   method: "PATCH",
+  //   headers: {"Content-type": "application/json"},
+  //   body: JSON.stringify({priority: event.target.value})
+  // })
+  // // // .then(res => res.json())
+  // // // .then(json => console.log(json))
+  // }
+
+
+
+
+  toyForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newToyName = document.getElementsByClassName('input-text')[0]
+    const newToyImage = document.getElementsByClassName('input-text')[1]
+    const newToyLikes = "0"
+    fetch("http://localhost:3000/toys",{
+       method: "POST",
+       headers: {"Content-type": "application/json"},
+       body: JSON.stringify({name: newToyName.value,
+         image: newToyImage.value,
+         likes: 0
+       })
+     })
+     .then(res => res.json())
+     .then((toy) => {
+       renderToy(toy)
+       newToyName.value = ""
+       newToyImage.value = ""
      })
    })
-   .then(res => res.json())
-   .then((toy) => {
-     renderToy(toy)
-     newToyName.value = ""
-     newToyImage.value = ""
-   })
 })
+
+
 // OR HERE!
