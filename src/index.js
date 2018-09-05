8d7af68f96ca88ab5d4b26cbc2e880929a58790a
ma539 <ma539@cornell.edu>
2018-09-05 11:35:31 -0400

    Code Challenge time up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ const toyForm = document.querySelector('.container')
 let addToy = false
 
 // YOUR CODE HERE
+document.addEventListener("DOMContentLoaded", () => {
+  let list = document.querySelector('#toy-collection')
+  fetch("http://localhost:3000/toys")
+  .then(res => {
+    return res.json()
+  })
+  .then(json => {
+    json.forEach((toy) => {
+      let toyDiv = document.createElement('div')
+      let toyName = document.createElement('h2')
+      let toyImage = document.createElement('img')
+      let toyLikes = document.createElement('p')
+      let likeButton = document.createElement('button')
+
+      toyDiv.className = 'card'
+      toyImage.className = 'toy-avatar'
+      likeButton.className = 'like-btn'
+
+      toyImage.src = toy.image
+      toyName.innerText = toy.name
+      toyLikes.innerText = toy.likes
+
+      toyDiv.append(toyName, toyImage, toyLikes, likeButton)
+      list.appendChild(toyDiv)
+    })
+  })
+})
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +42,22 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-
+toyForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const newToyName = document.getElementbyId('name')
+  const newToyImage = document.getElementbyId('image')
+  fetch("http://localhost:3000/toys",{
+     method: "POST",
+     headers: {"Content-type": "application/json"},
+     body: JSON.stringify({name: newToyName.value,
+       image: newToyImage.value,
+     })
+   })
+   .then(res => res.json())
+   .then((toy) => {
+     renderToy(toy)
+     newToyName.value = ""
+     newToyImage.value = ""
+   })
+})
 // OR HERE!
